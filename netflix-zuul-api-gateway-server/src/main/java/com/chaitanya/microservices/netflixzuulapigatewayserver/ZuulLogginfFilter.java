cb5c50b2b7aa2a026f5e9c1877a978505beaa4b6
Chaitanya Dehankar <chaitanyadehankar@gmail.com>
2020-08-18 10:12:40 +0530

    Adding Zull api gateway server

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/chaitanya/microservices/netflixzuulapigatewayserver/ZuulLogginfFilter.java
@@ -0,0 +1,44 @@
+package com.chaitanya.microservices.netflixzuulapigatewayserver;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLogginfFilter extends ZuulFilter {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		logger.info("request = {} request uri = {}", request, request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
