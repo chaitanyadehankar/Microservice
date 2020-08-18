f1898054171703768caeefb73877d848d8c75476
Chaitanya Dehankar <chaitanyadehankar@gmail.com>
2020-08-18 10:10:49 +0530

    Adding Eureka Naming Server

---
+++ netflix-eureka-naming-server/src/test/java/com/chaitanya/microservices/netflixeurekanaming/server/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaitanya.microservices.netflixeurekanaming.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
